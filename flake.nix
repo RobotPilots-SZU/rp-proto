8da9e5ae7375f30591821a08adef5f5fb1c6d4d5
vix_hentx <w1084349470@outlook.com>
2026-05-30 21:35:04 +0800

    nix,buf: 添加nix环境以及buf语法检查

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "RobotPilots Unified Protocol Repository";
+
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ];
+      imports = [
+        ./nix/shells
+        ./nix/check.nix
+      ];
+    };
+}
